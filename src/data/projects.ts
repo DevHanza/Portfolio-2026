3927dc723d6dcbe28a1cc1c5b4dcf77de72561de
DevHanza <hansana123dasanayake@gmail.com>
2026-05-06 14:35:38 +0530

    Add "image" & "url" to the Projects Data and integrate them

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,24 +1,34 @@
+import cafesy from "../assets/ProjectThumbs/Cafesy.png";
+
 export const projects = {
   personal: [
     {
       title: "Habitzy",
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, minus.",
       stack: ["React", "Node.js"],
+      image: cafesy,
+      url: "/",
     },
     {
       title: "TelegramYTDLBot  —  Telegram Bot",
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, minus.",
       stack: ["Python"],
+      image: cafesy,
+      url: "/",
     },
     {
       title: "SchoolNotes  —  MEAN-stack Application",
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, minus.",
       stack: ["Angular"],
+      image: cafesy,
+      url: "/",
     },
     {
       title: "Cafesy  —  E-Commerce Website",
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, minus.",
       stack: ["Node.js"],
+      image: cafesy,
+      url: "https://github.com/DevHanza/cafesy",
     },
   ],
   commercial: [
@@ -33,6 +43,8 @@ export const projects = {
         "React",
         "Node.js",
       ],
+      image: cafesy,
+      url: "/",
     },
   ],
 };
